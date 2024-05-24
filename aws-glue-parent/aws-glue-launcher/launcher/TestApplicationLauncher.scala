12f7414aa85d6a7813f3983db4f2a1337bfe9bb3
maneldt <maneldt@cakeup.app>
2024-05-24 12:34:22 +0200

    Setup done

---
+++ aws-glue-parent/aws-glue-launcher/launcher/TestApplicationLauncher.scala
@@ -0,0 +1,28 @@
+import com.amazonaws.services.glue.GlueContext
+import com.amazonaws.services.glue.util.GlueArgParser
+import com.amazonaws.services.glue.util.Job
+import java.util.Calendar
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.from_json
+import org.apache.spark.sql.streaming.Trigger
+import scala.collection.JavaConverters._
+
+object TestApplicationLauncher{
+  def main(sysArgs: Array[String]) {
+    val spark: SparkContext = new SparkContext()
+    val glueContext: GlueContext = new GlueContext(spark)
+    val sparkSession: SparkSession = glueContext.getSparkSession
+    import sparkSession.implicits._
+    // @params: [JOB_NAME]
+    val args = GlueArgParser.getResolvedOptions(sysArgs, Seq("JOB_NAME").toArray)
+    Job.init(args("JOB_NAME"), glueContext, args.asJava)
+
+    val testApplication: TestApplication = TestApplication(args)
+
+    Job.commit()
+  }
+}
---
+++ aws-glue-parent/aws-glue/src/main/scala/com/mduenast/TestApplication.scala
@@ -0,0 +1,14 @@
+package com.mduenast
+
+class TestApplication {
+  def main(args: Map[String, String]): Unit = {
+    //TODO:
+  }
+
+}
+
+object TestApplication {
+  def apply(args: Map[String, String]): TestApplication = {
+    new TestApplication
+  }
+}
